a4083989a490b60767920875da67d837ab476c67
RyoWatanabe-1980 <98007413+RyoWatanabe-1980@users.noreply.github.com>
2023-12-30 17:08:51 +0900

    welcomeからmenuへ遷移

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <!-- Google アイコンフォントの読み込み -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- モバイル最適化のための設定 -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+
+<body>
+    <!-- ページ全体を包むwrapper要素 -->
+
+    <!-- ナビゲーションバー -->
+    <nav id="navigation">
+        <div class="nav-wrapper">
+            <a href="#" class="nav-title">タイトル</a>
+        </div>
+    </nav>
+
+    <!-- メインコンテンツ -->
+    <div id="main" class="main-bottom-64">
+        <div class="container main-container-vertical">
+            <div class="wrapper">
+                <div class="row">
+                    <div class="col s6 m3">
+                        <div class="card blue-grey darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title">クイズ１</span>
+                                <p>４つなかからえらんでね
+                                </p>
+                            </div>
+                            <div class="card-action">
+                                <a href="./quiz1.html">すすむ</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s6 m3">
+                        <div class="card blue-grey darken-1">
+                            <div class="card-content white-text">
+                                <span class="card-title">クイズ２</span>
+                                <p>まちがっているところをタップしてね
+                                </p>
+                            </div>
+                            <div class="card-action">
+                                <a href="./quiz2.html">すすむ</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6 m3">
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">Card Title</span>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                    </p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">クイズを始める</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s6 m3">
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">Card Title</span>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                    </p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">クイズを始める</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col s6 m3">
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">Card Title</span>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                    </p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">クイズを始める</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col s6 m3">
+                            <div class="card blue-grey darken-1">
+                                <div class="card-content white-text">
+                                    <span class="card-title">Card Title</span>
+                                    <p>I am a very simple card. I am good at containing small bits of information.
+                                    </p>
+                                </div>
+                                <div class="card-action">
+                                    <a href="#">クイズを始める</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- フッター -->
+    <footer id="footer" class="page-footer padding-top-0">
+        <!-- フッターコンテンツ -->
+        <div class="footer-copyright">
+            <div class="container">
+
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Refresh" content="3;/menu">
+    <link rel="stylesheet" href="{{ asset('/css/style.css') }}">
+    <title>welcome</title>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <div class="logo">
+            <img src="{{asset('storage/logo.png')}}">
+        </div>
+    </div>   
+</div>
+</body>
+</html>
